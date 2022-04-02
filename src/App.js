a9fb7dbe78754e34904866574c7a3c804f4edac3
chibah-fawzi <fawzichibah@gmail.com>
2022-04-02 16:38:01 +0100

    last

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import { Link, Outlet } from "react-router-dom";
 function App() {
 
   const [editions, setEditions] = useState([]);
+  const [loading, setLoading] = useState(false);
   const getEditions = async () => {
+    setLoading(true)
     const res = await axios.get('http://api.alquran.cloud/v1/edition', {
       params: {
 
       }
     })
     setEditions(res.data.data);
+    setLoading(true)
   }
 
   useEffect(() => {
@@ -27,20 +30,22 @@ function App() {
 
       <h2>List of Quran editions</h2>
       <div className='row'>
-        {editions.map(e => {
-          return (
-            <div className='col-4 mt-5 text-left'>
-              <div>
-                <Link key={e.identifier} to={`/edition/${e.identifier}`}><h3>{e.name}</h3></Link>
-                <Link key={e.identifier} to={`/edition/${e.identifier}`}><h6>{e.englishName}</h6></Link>
-              </div>
-              <ul>
-                <li>Language : {e.language}</li>
-                <li>Direction : {e.direction}</li>
-              </ul>
-            </div>
-          )
-        })}
+        {
+          loading ? <p>Loading ...</p> :
+            editions.map(e => {
+              return (
+                <div className='col-4 mt-5 text-left'>
+                  <div>
+                    <Link key={e.identifier} to={`/edition/${e.identifier}`}><h3>{e.name}</h3></Link>
+                    <Link key={e.identifier} to={`/edition/${e.identifier}`}><h6>{e.englishName}</h6></Link>
+                  </div>
+                  <ul>
+                    <li>Language : {e.language}</li>
+                    <li>Direction : {e.direction}</li>
+                  </ul>
+                </div>
+              )
+            })}
       </div>
       <Outlet />
     </div>
